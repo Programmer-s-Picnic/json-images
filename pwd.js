60e03c155fdb631658d52eb7d62a251693874147
Champak's World <champaksworld@gmail.com>
2026-04-07 18:15:33 +0530

    Create pwd.js

---
+++ pwd.js
@@ -0,0 +1,7 @@
+<script>
+  window.pppPwdConfig = {
+    password: "12345",
+    title: "Protected Lesson",
+    subtitle: "Enter password to open this page",
+    storageKey: "ppp_java_lesson_access"
+  };
